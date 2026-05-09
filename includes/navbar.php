a57a2ba8815fa32b4b8905259919db0aed3754df
Umer Khatab <umersaqi1@gmail.com>
2026-05-09 21:38:48 +0500

    Initial commit

---
+++ includes/navbar.php
@@ -0,0 +1,42 @@
+<nav class="navbar py-4 navbar-expand-lg navbar-dark bg-primary spinnaker-regular  sticky-top">
+  <div class="container-fluid">
+
+    <!-- Toggler button for mobile screens -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Menus on the left + Sign Up button in collapse on mobile screens -->
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active spinnaker-regular" aria-current="page" href="https://thepearlgscmi.com/">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active spinnaker-regular" aria-current="page" href="academic.php">ACADEMIC</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active spinnaker-regular" aria-current="page" href="about.php">ABOUT US</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active spinnaker-regular" aria-current="page" href="contact.php">CONTACT US</a>
+        </li>
+      </ul>
+
+      <!-- Signup button - moves after menu items in mobile view -->
+      <div class="d-lg-none mt-3">
+        <button class="btn btn-outline-success  spinnaker-regular btn-sm px-5" type="button">Admission Form</button>
+      </div>
+    </div>
+
+    <!-- Logo in the center -->
+    <a class="navbar-brand position-absolute start-50 translate-middle-x" href="#">
+      <img src="webImages/logo.png" alt="Logo" height="85">
+    </a>
+
+    <!-- Signup button on the right for larger screens -->
+    <div class="d-none d-lg-flex ms-auto">
+      <button class="btn btn-outline-light  spinnaker-regular btn-sm px-5" type="button">Admission Form</button>
+    </div>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,313 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Spinnaker&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <link rel="stylesheet" type="text/css" href="assets/style.css">
+    <link rel="icon" href="webImages/logo.png" sizes="32x32" />
+    
+    <!-- Swiper CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/slider.css">
+    <title>The pearls Group of Schools, Colleges &amp; Medical Sciences &#8211; Empowering Minds, Shaping Futures</title>
+
+  </head>
+  <body>
+      <!--Navbar-->
+<?php include "includes/navbar.php"; ?>
+<!--header-->
+<section style="position: relative; height: 100vh; overflow: hidden;">
+    <div style="background-image: url('webImages/homeheaderimg.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center; position: absolute; top: 0; left: 0; right: 0; bottom: 0; opacity: 0.5;"></div>
+    <div class="container-fluid position-relative">
+        <div class="row">
+            <div class="col-12 text-center pt-5 mt-5">
+                <div class="background-div">
+                    <h1 class="my-5 py-5 spinnaker-regular">The Superior College Bhakkar</h1>
+                </div>
+            </div>
+            <div class="col-12 text-center">
+                <button class="btn btn-outline-success btn-lg">OUR ACADEMICS</button>
+                <button class="btn btn-success btn-lg">ABOUT US</button>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <div class="row text-center px-4">
+            <div class="col-4 card">
+                <center><img src="webImages/starts.png" class="img-fluid p-3 iconImages"></center>
+                <h5 class="spinnaker-regular">Academic Excellence</h5>
+                <p class="p-3">
+                    We consistently prioritize and achieves high standards of academic excellence.
+                </p>
+            </div>
+            <div class="col-4 cardc pt-5">
+                <center class="pt-3"><img src="webImages/books.png" class="img-fluid p-3 iconImages"></center>
+                <h5 class="spinnaker-regular">Interactive Learning</h5>
+                <p class="p-3">
+                    We promote inclusivity by valuing diversity and creating an environment where all students feel welcome and supported.
+                </p>
+            </div>
+            <div class="col-4 card">
+                <center><img src="webImages/msg.png" class="img-fluid p-3 iconImages"></center>
+                <h5 class="spinnaker-regular">Effective Communication</h5>
+                <p class="p-3">
+                    We prioritize clear and open communication among all stakeholders, including students, faculty, staff, and parents.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<center><hr class="smallhr"></center>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h4 class="text-center spinnaker-regular">Check out our latest promos!</h4>
+            </div>
+        </div>
+    </div>
+</section>
+<center>
+    <img src="webImages/shr.png" class="img-fluid py-2"> 
+</center>
+
+<!--slider-->
+<section>
+    <div class="slide-container swiper">
+    <div class="slide-content">
+      <div class="card-wrapper swiper-wrapper">
+        <div class="card swiper-slide">
+          <div class="card-content">
+              <img src="sliderImages/s1.jpg" alt="" class="card-img">
+          </div>
+        </div>
+        <div class="card swiper-slide">
+          <div class="card-content">
+              <img src="sliderImages/s2.jpg" alt="" class="card-img">
+          </div>
+        </div>
+        <div class="card swiper-slide">
+          <div class="card-content">
+              <img src="sliderImages/s3.jpg" alt="" class="card-img">
+          </div>
+        </div>
+        <div class="card swiper-slide">
+          <div class="card-content">
+              <img src="sliderImages/s4.jpg" alt="" class="card-img">
+          </div>
+        </div>
+        <div class="card swiper-slide">
+          <div class="card-content">
+              <img src="sliderImages/s5.jpg" alt="" class="card-img">
+          </div>
+        </div>
+        <div class="card swiper-slide">
+          <div class="card-content">
+              <img src="sliderImages/s6.jpg" alt="" class="card-img">
+          </div>
+        </div>
+        <div class="card swiper-slide">
+          <div class="card-content">
+              <img src="sliderImages/s1.jpg" alt="" class="card-img">
+          </div>
+        </div>
+        <div class="card swiper-slide">
+          <div class="card-content">
+              <img src="sliderImages/s2.jpg" alt="" class="card-img">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="swiper-button-next swiper-navBtn"></div>
+    <div class="swiper-button-prev swiper-navBtn"></div>
+    <div class="swiper-pagination"></div>
+  </div>
+</section>
+<section>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-lg-6 col-sm-12 pt-5">
+                <img class="img-fluid float-end" src="webImages/pakmap.png">
+            </div>
+            <div class="col-lg-6 col-sm-12">
+                <h1 class="spinnaker-regular mt-5 pt-5">Our Campuses</h1>
+                <ul class="ul">
+                   <li class="li">Superior College Campuse - Lahore Zone</li>
+                    <li class="li">Superior College Campuse - Sialkot Zone</li>
+                    <li class="li">Superior College Campuse - Gujranwala Zone</li>
+                    <li class="li">Superior College Campuse - DG-Khan Zone</li>
+                    <li class="li">Superior College Campuse - Bahawalpur Zone</li>
+                    <li class="li">Superior College Campuse - Okara Zone</li>
+                    <li class="li">Superior College Campuse - Sargodha Zone</li>
+                    <li class="li">Superior College Campuse - Faisalabad Zone</li>
+                    <li class="li">Superior College Campuse - Rawalpindi Zone</li>
+                    <li class="li">Superior College Campuse - Khyber Pakhtunkhwa Zone</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container mt-5 pt-5">
+        <div class="row">
+            <div class="col-12">
+                <h4 class="text-center spinnaker-regular">Why Choose Us?</h4>
+            </div>
+        </div>
+    </div>
+</section>
+<center>
+    <img src="webImages/shr.png" class="img-fluid py-2"> 
+</center>
+
+<section>
+    <div class="container mt-5">
+        <div class="row">
+             <div class="col-lg-4 col-sm-12 col-md-6 p-3 text-center">
+                 <div class="bshadow">
+                     <center><img src="webImages/hand.png" class="img-fluid p-3 iconImages"></center>
+                     <h2 class="spinnaker-regular p-2">Exceptional Reputation</h2>
+                     <p class="p-4">
+                         We take immense pride in our unwavering commitment to academic excellence. Our rigorous curriculum is designed to challenge and inspire students, providing them with a solid foundation of knowledge and skills.
+                     </p>
+                 </div>
+                 
+             </div>
+             <div class="col-lg-4 col-sm-12 col-md-6 p-3 text-center">
+                 <div class="bshadow">
+                     <center><img src="webImages/build.png" class="img-fluid p-3 iconImages"></center>
+                     <h2 class="spinnaker-regular p-2">Cutting-Edge Facilities</h2>
+                     <p class="p-4">
+                         When you choose us, you choose an institution that seamlessly blends academic theory with real-world application.
+                     </p>
+                 </div>
+                 
+             </div>
+             <div class="col-lg-4 col-sm-12 col-md-6 p-3 text-center">
+                 <div class="bshadow">
+                     <center><img src="webImages/head.png" class="img-fluid p-3 iconImages"></center>
+                     <h2 class="spinnaker-regular p-2">Holistic Development Focus</h2>
+                     <p class="p-4">
+                         We understand that education is not just about textbooks and exams It's about nurturing well-rounded individuals. Our institute is committed to the holistic development of our students
+                     </p>
+                 </div>
+                 
+             </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container mt-5 pt-5">
+        <div class="row">
+            <div class="col-lg-6 col-sm-12 pt-5">
+                <b class="text-success spinnaker-regular">Superior College</b>
+                <h2 class="spinnaker-regular">Where Learning Begins</h2>
+                <br>
+                <div class="d-flex">
+                <span class="fa fa-shield text-primary" style="font-size:24pt;"></span> &emsp; <h4><b>Co-Corricular Activities</b></h4></div>
+                <br><br>
+                <div class="d-flex">
+                <span class="fa fa-shield text-primary" style="font-size:24pt;"></span> &emsp; <h4><b>Competitions & Sports</b></h4></div>
+                
+                <br><br>
+                <button class="btn btn-success px-4 btn-lg">About Us</button>
+            </div>
+            <div class="col-lg-6 col-sm-12">
+                <img src="webImages/sports.jpg" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section style="background-image: url('webImages/abcd.PNG');">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-sm-12">
+                
+            </div>
+            <div class="col-lg-6 col-sm-12">
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+<section class="p-3 mt-5 text-center footer">
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+    
+                <!-- About -->
+                <div class="col-md-3">
+                    <h5 class="footer-title">About</h5>
+                    <p>
+                        The Superior Group of Colleges is committed to quality education 
+                        and student development across multiple campuses.
+                    </p>
+                </div>
+    
+                <!-- Quick Links -->
+                <div class="col-md-3">
+                    <h5 class="footer-title">Quick Links</h5>
+                    <ul class="footer-links">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Programs</a></li>
+                        <li><a href="#">Admissions</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+    
+                <!-- Campuses -->
+                <div class="col-md-3">
+                    <h5 class="footer-title">Campuses</h5>
+                    <ul class="footer-links">
+                        <li>Lahore Zone</li>
+                        <li>Sialkot Zone</li>
+                        <li>Gujranwala Zone</li>
+                        <li>DG-Khan Zone</li>
+                    </ul>
+                </div>
+    
+                <!-- Contact -->
+                <div class="col-md-3">
+                    <h5 class="footer-title">Contact</h5>
+                    <p>Email: info@thepearlgscmi.com</p>
+                    <p>Phone: +92-XXX-XXXXXXX</p>
+                    <p>Pakistan</p>
+                </div>
+    
+            </div>
+        </div>
+    
+        <!-- Bottom Bar -->
+        <div class="footer-bottom">
+            <p>© 2026 The Pearl Group. All Rights Reserved.</p>
+        </div>
+    </footer>
+</section>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="assets/slider.js"></script>
+
+  </body>
+</html>
